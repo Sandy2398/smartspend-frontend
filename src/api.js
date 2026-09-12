43abcb5b430c2e53afd34f88fe86c9fd724cd3ee
Santhosh <santhoshraj2398@gmail.com>
2026-09-12 12:21:02 +0300

    Connect frontend to deployed backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://smartspend-backend1.onrender.com/api",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
